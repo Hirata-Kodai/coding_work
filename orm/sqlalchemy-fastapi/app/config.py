93efa2cae1d6e95b17a0d5e01517df0280155439
Hirata-Kodai <hiratako@ist.hokudai.ac.jp>
2022-08-10 20:04:59 +0900

    初期データベースの作成を環境変数で設定

--- orm/sqlalchemy-fastapi/app/config.py
+++ orm/sqlalchemy-fastapi/app/config.py
@@ -4,10 +4,10 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     allow_origins: List[str] = ["http://localhost:3000"]  # これは React とかを使うとき用
-    db_name: str
+    MYSQL_DATABASE: str
     db_host: str
     db_user: str
-    db_password: str
+    MYSQL_ROOT_PASSWORD: str
 
     class Config:
         env_file = ".env"
--- orm/sqlalchemy-fastapi/app/database.py
+++ orm/sqlalchemy-fastapi/app/database.py
@@ -5,9 +5,8 @@ from config import Settings
 
 
 setting = Settings()
-# この時点では存在していないデータベースを選択しても問題ない
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://" +\
-    f"{setting.db_user}:{setting.db_password}@{setting.db_host}/{setting.db_name}"
+    f"{setting.db_user}:{setting.MYSQL_ROOT_PASSWORD}@{setting.db_host}/{setting.MYSQL_DATABASE}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
